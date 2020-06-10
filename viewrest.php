149b78354196b2c2ccc697dd0e25bffada06f480
bkisgithub <66293920+bkisgithub@users.noreply.github.com>
2020-06-10 12:21:12 +0800

    Update viewrest.php

--- viewrest.php
+++ viewrest.php
@@ -8,4 +8,28 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     die("Error connection: ".sqlsrv_errors());
   }
   echo "Connection to Db: Success!";
+
+$tsql= "SELECT * FROM [dbo].[restaurant]";
+ $getResults= sqlsrv_query($conn, $tsql);
+
+if ($getResults == FALSE)
+{
+ die(sqlsrv_errors());
+}
+
+echo "<table border = '1'>";
+
+ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+ echo "<tr>";
+ echo "<td>". $row['restaurant_id'] . "</td>";
+ echo "<td>". $row['restaurant_name'] ."</td>";
+ echo "<td>". $row['restaurant_address'] . "</td>";
+ echo "<td>". $row['restaurant_phone'] . "</td>";
+ echo "</tr>";
+ }
+ echo "</table>";
+
+ sqlsrv_free_stmt($getResults);
+?>
 ?>
