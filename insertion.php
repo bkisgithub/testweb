df152132422bff06da44587157c2404101912f21
bkisgithub <66293920+bkisgithub@users.noreply.github.com>
2020-06-10 12:29:20 +0800

    Update insertion.php

--- insertion.php
+++ insertion.php
@@ -1,6 +1,6 @@
 <?php
   // SQL Server Extension Sample Code:
-  $connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@123456", 
+  $connectionInfo = array("UID" => "apuadmin", "pwd" => "Apu12345", 
   "Database" => "WebApplicationDb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
   $serverName = "tcp:webapplicationtp000000.database.windows.net,1433";
   $conn = sqlsrv_connect($serverName, $connectionInfo);
